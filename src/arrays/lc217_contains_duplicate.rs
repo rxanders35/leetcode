595d38a217796c7cd14df296d00be0ef8695ac84
rxanders35 <reidxanders@outlook.com>
2024-10-31 15:04:59 -0400

    lc217 - "Contains Duplicates"

--- src/arrays/lc217_contains_duplicate.rs
+++ src/arrays/lc217_contains_duplicate.rs
@@ -3,11 +3,10 @@ use std::collections::HashSet;
 pub fn contains_duplicate(nums: Vec<i32>) -> bool {
     let mut hashset = HashSet::new();
     for i in nums {
-        if !hashset.contains(&i){
-            hashset.insert(i)
-        } else {
-            //figure this out
+        if hashset.contains(&i) {
+            return true
         }
+        hashset.insert(i);
     }
-    true
+    false
 }
